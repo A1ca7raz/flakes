48b770fda0bcaad9aa9ab3f97741823053d2b75c
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-02-20 06:27:05 +0800

    desktop: hotfix applet-window-buttons6

--- modules/desktop/plasma/desktop/applets.nix
+++ modules/desktop/plasma/desktop/applets.nix
@@ -1,7 +1,13 @@
 { home, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    kdePackages.applet-window-buttons6
+    (kdePackages.applet-window-buttons6.overrideAttrs (p: {
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://github.com/moodyhunter/applet-window-buttons6/pull/22.patch";
+        })
+      ];
+    }))
     plasma-panel-colorizer-nightly
     plasma-panel-spacer-extended
     kara
