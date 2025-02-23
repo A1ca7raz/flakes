e67bc94c825db9f32ff4c6b25a366a2572778e7e
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-02-24 02:38:37 +0800

    rclone: clean

--- modules/services/client/rclone/default.nix
+++ modules/services/client/rclone/default.nix
@@ -9,7 +9,18 @@
       mkRcloneService = { targetPath, sourcePath }:
         let
           mountPath = ".cache/rclone/${targetPath}";
-          rcloneOptions = import ./options.nix;
+          rcloneOptions = builtins.concatStringsSep " " [
+            "--multi-thread-streams 64"
+            "--multi-thread-cutoff 128M"
+            "--vfs-cache-mode full"
+            "--vfs-cache-max-size 100G"
+            "--vfs-cache-max-age 6h"
+            "--vfs-read-chunk-size-limit off"
+            "--buffer-size 128M"
+            "--vfs-read-chunk-size 128M"
+            "--vfs-read-wait 0ms"
+            "--allow-non-empty"
+          ];
         in {
           Unit = {
             Description = "Rclone Daemon Auto mount";
@@ -45,4 +56,3 @@
       };
     };
 }
-
