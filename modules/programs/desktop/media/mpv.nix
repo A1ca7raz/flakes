6952937b131be04952897c3225fecfda45dd13f8
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-02-23 17:51:31 +0800

    mpv: update config

--- modules/programs/desktop/media/mpv.nix
+++ modules/programs/desktop/media/mpv.nix
@@ -15,42 +15,46 @@ with pkgs; let
   };
 in {
   lib.packages.mpv = mpvPackage;
-  home.packages = [ mpvPackage ];
-  xdg.configFile.mpvConf = {
-    target = "mpv/mpv.conf";
-    text = ''
-      profile=gpu-hq
-      scale=ewa_lanczossharp
-      cscale=ewa_lanczossharp
-      video-sync=display-resample
-      interpolation
-      tscale=oversample
-      save-position-on-quit
-      hwdec=auto
-      volume-max=150
-    '';
-  };
-  xdg.configFile.mpvInputConf = {
-    target = "mpv/input.conf";
-    text = ''
-      [	add speed -0.25
-      ]	add speed 0.25
-    '';
-  };
-  xdg.configFile.mpvUoscConf = {
-    target = "mpv/script-opts/uosc.conf";
-    text = ''
-      timeline_style=bar
-      timeline_size=25
-      timeline_opacity=0.7i
 
-      controls_margin=4
-      controls_spacing=4
+  programs.mpv = {
+    enable = true;
+    package = mpvPackage;
 
-      volume_size=35
-      volume_size_fullscreen=40
-      volume_opacity=0.5
-      volume_step=5
-    '';
+    config = {
+      scale = "ewa_hanning";
+      cscale = "bilinear";
+      dscale = "ewa_hanning";
+      video-sync = "display-resample";
+      interpolation = true;
+      save-position-on-quit = true;
+      hwdec = "auto-copy";
+      hwdec-codecs = "all";
+      volume-max = 150;
+
+      # Network stream cache
+      demuxer-max-bytes = "400MiB";       # forward cache
+      demuxer-max-back-bytes = "100MiB";  # At least 100MiB memory space for backward cache
+      demuxer-donate-buffer = "yes";      # Allow backward cache 'borrow' space from forward cache (for live stream, total 500MiB available for backward cache)
+      demuxer-hysteresis-secs = 10;       # extra cache when 500MiB run out
+    };
+
+    bindings = {
+      "[" =	"add speed -0.25";
+      "]" =	"add speed 0.25";
+    };
+
+    scriptOpts.uosc = {
+      timeline_style = "bar";
+      timeline_size = 25;
+      timeline_opacity = 0.7;
+
+      controls_margin = 4;
+      controls_spacing = 4;
+
+      volume_size = 35;
+      volume_size_fullscreen = 40;
+      volume_opacity = 0.5;
+      volume_step = 5;
+    };
   };
 }
