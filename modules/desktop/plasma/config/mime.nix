e6ef2ce7b66c560f386a90e22e5a33fc28c6104e
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-03-20 00:28:26 +0800

    plasma: clean mime

--- modules/desktop/plasma/config/mime.nix
+++ modules/desktop/plasma/config/mime.nix
@@ -7,22 +7,18 @@ let
   # Normal Apps
   kate = mkApp "org.kde.kate";
   vsc = mkApp "codium";                          # VSCode (VSCodium)
-  ghostwriter = mkApp "org.kde.ghostwriter";     # Default Markdown Editor
-  chromium = mkApp "chromium-browser";
   firefox = mkApp "firefox";
   mpv = mkApp "mpv";
-  vlc = mkApp "vlc";
 in {
   xdg.mimeApps = {
     enable = true;
     associations.added = {
-      "text/markdown" = [ ghostwriter kate vsc ];
+      "text/markdown" = [ kate vsc ];
       "application/x-zerosize" = [ kate ];
     };
 
     defaultApplications = {
-      "text/markdown" = ghostwriter;
-      "text/plain"    = kate;
+      "text/plain" = kate;
       "video/mp4" = mpv;
       "application/pdf" = firefox;
       "application/x-zerosize" = kate;
