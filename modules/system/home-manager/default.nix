db226d41c8f336631becf80bbb7d6a979e19a3ec
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-03-20 00:39:11 +0800

    system: add gc for home-manager

--- modules/system/home-manager/default.nix
+++ modules/system/home-manager/default.nix
@@ -5,4 +5,9 @@
   # programs.home-manager.enable = true;
   # https://github.com/nix-community/home-manager/issues/3211
   home.packages = [ pkgs.home-manager ];
+
+  nix.gc = {
+    automatic = true;
+    frequency = "weekly";
+  };
 }
