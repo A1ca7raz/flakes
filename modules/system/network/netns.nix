3931118d820e4a7c84beb682056c163d405a1b38
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-12 01:47:45 +0800

    network: add forwarding for netns

--- modules/system/network/netns.nix
+++ modules/system/network/netns.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
   utils.netns.enable = true;
+  utils.netns.bridge."0".ipAddrs = lib.mkDefault [ "10.0.0.254/24" ];
+
+  # https://lantian.pub/article/modify-website/dn42-experimental-network-2020.lantian/#%E9%9D%9E%E5%B8%B8%E9%87%8D%E8%A6%81%E7%9A%84%E7%B3%BB%E7%BB%9F%E9%85%8D%E7%BD%AE
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.default.forwarding" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+    "net.ipv4.conf.default.rp_filter" = 0;
+    "net.ipv4.conf.all.rp_filter" = 0;
+  };
 }
