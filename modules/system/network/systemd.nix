af8e977cddb9ea46cd6993597519b071316e235a
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-11 17:36:14 +0800

    system: use DHCP for systemd-networkd

--- modules/system/network/systemd.nix
+++ modules/system/network/systemd.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, lib, ... }:
 {
-  # 使用 systemd-networkd 管理网络
   systemd.network.enable = true;
   services.resolved.enable = false;
 
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
+  systemd.network.networks.default =  {
+    DHCP = "yes";
+    matchConfig.Name = "eth0";
+    networkConfig.IPv4Forwarding = lib.optionalAttrs config.utils.netns.enable "yes";
+  };
 }
