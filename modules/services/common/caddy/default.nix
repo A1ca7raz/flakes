72c822921a01f648c1f4cbdbb19196b45ead2eb1
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-13 00:12:09 +0800

    services: harden caddy service

--- modules/services/common/caddy/default.nix
+++ modules/services/common/caddy/default.nix
@@ -21,7 +21,20 @@
     bindsTo = [ "netns-veth-caddy.service" ];
 
     unitConfig.JoinsNamespaceOf = [ "netns@proxy.service" ];
-    serviceConfig.BindReadOnlyPaths = ["/etc/netns/proxy/resolv.conf:/etc/resolv.conf"];
-    serviceConfig.PrivateNetwork = true;
+    serviceConfig = {
+      BindReadOnlyPaths = ["/etc/netns/proxy/resolv.conf:/etc/resolv.conf"];
+      PrivateNetwork = true;
+
+      # Light-weight Harden
+      AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+      CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+      LimitNOFILE = 1048576;
+      PrivateTmp = true;
+      ProtectSystem = "strict";
+      ProtectProc = "invisible";
+      RemoveIPC = true;
+      ProtectControlGroups = true;
+      ProtectKernelTunables = true;
+    };
   };
 }
