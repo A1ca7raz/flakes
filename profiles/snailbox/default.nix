0b2eda777ef38dc1e3c34e196834b1741aed76ab
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-13 00:13:00 +0800

    snailbox: update basic services

--- profiles/snailbox/default.nix
+++ profiles/snailbox/default.nix
@@ -1,8 +1,8 @@
-{ self, lib, templates, ... }:
+{ self, lib, templates, variables, ... }:
 {
   imports = [ templates.vps ];
 
-  targetHost = "192.168.10.168";
+  targetHost = "192.168.1.100";
   hostName = "oxygenbox";
   tags = with lib.tags; [
     local internal private
@@ -15,35 +15,42 @@
     system.bootloader.efi.grub.removable
     system.kernel.xanmod
 
-    # {
-    #   # PostgreSQL Tune
-    #   # https://pgtune.leopard.in.ua/
-    #   services.postgresql.settings = {
-    #     # DB Version: 17
-    #     # OS Type: linux
-    #     # DB Type: mixed
-    #     # Total Memory (RAM): 4 GB
-    #     # CPUs num: 4
-    #     # Connections num: 100
-    #     # Data Storage: ssd
-    #     max_connections = 100;
-    #     shared_buffers = "1GB";
-    #     effective_cache_size = "3GB";
-    #     maintenance_work_mem = "256MB";
-    #     checkpoint_completion_target = 0.9;
-    #     wal_buffers = "16MB";
-    #     default_statistics_target = 100;
-    #     random_page_cost = 1.1;
-    #     effective_io_concurrency = 200;
-    #     work_mem = "5041kB";
-    #     huge_pages = "off";
-    #     min_wal_size = "1GB";
-    #     max_wal_size = "4GB";
-    #     max_worker_processes = 4;
-    #     max_parallel_workers_per_gather = 2;
-    #     max_parallel_workers = 4;
-    #     max_parallel_maintenance_workers = 2;
-    #   };
-    # }
+    services.common.postgresql
+    services.common.redis
+    services.common.caddy
+    services.common.knot
+
+    {
+      # PostgreSQL Tune
+      # https://pgtune.leopard.in.ua/
+      services.postgresql.settings = {
+        # DB Version: 17
+        # OS Type: linux
+        # DB Type: mixed
+        # Total Memory (RAM): 4 GB
+        # CPUs num: 4
+        # Connections num: 100
+        # Data Storage: ssd
+        max_connections = 100;
+        shared_buffers = "1GB";
+        effective_cache_size = "3GB";
+        maintenance_work_mem = "256MB";
+        checkpoint_completion_target = 0.9;
+        wal_buffers = "16MB";
+        default_statistics_target = 100;
+        random_page_cost = 1.1;
+        effective_io_concurrency = 200;
+        work_mem = "5041kB";
+        huge_pages = "off";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = 4;
+        max_parallel_workers_per_gather = 2;
+        max_parallel_workers = 4;
+        max_parallel_maintenance_workers = 2;
+      };
+    }
+
+    { utils.netns.bridge."0".ipAddrs = variables.homelab.vnet.ipAddrs; }
   ];
 }
