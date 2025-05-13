c878ef275500dac998ff34111b22ec5d3be0856f
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-14 00:57:59 +0800

    system: remove unused bridge

--- modules/system/network/netns.nix
+++ modules/system/network/netns.nix
@@ -1,7 +1,6 @@
-{ lib, ... }:
+{ ... }:
 {
   utils.netns.enable = true;
-  utils.netns.bridge."0".ipAddrs = lib.mkDefault [ "10.0.0.254/24" ];
 
   # https://lantian.pub/article/modify-website/dn42-experimental-network-2020.lantian/#%E9%9D%9E%E5%B8%B8%E9%87%8D%E8%A6%81%E7%9A%84%E7%B3%BB%E7%BB%9F%E9%85%8D%E7%BD%AE
   boot.kernel.sysctl = {
