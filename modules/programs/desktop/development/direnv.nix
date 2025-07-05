61445f94ac6b692dca379bcc8a7e7504b5885779
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-07-05 17:48:32 +0800

    program(direnv): add auto cleaner of direnv drv

--- modules/programs/desktop/development/direnv.nix
+++ modules/programs/desktop/development/direnv.nix
@@ -1,13 +1,21 @@
 {
-  homeModule = { ... }: {
+  homeModule = { lib, ... }: {
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
     };
   };
 
-  nixosModule = { user, lib, ... }:
-    with lib; mkPersistDirsModule user [
+  nixosModule = { user, lib, pkgs, ... }: with lib; {
+    environment.persistence = mkPersistDirsTree user [
       (c "direnv") (ls "direnv")
     ];
+
+    system.activationScripts.cleanDirenv = ''
+      function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+      if [[ -e /run/current-system ]] && version_gt $(cat $systemConfig/nixos-version) $(cat /run/current-system/nixos-version); then
+        rm -v $(nix-store --gc --print-roots --quiet | egrep -v "^(/nix/var|/run/\w+-system|\{memory|/proc)" | egrep ".direnv" | egrep -v "^Removing" | cut -f1 -d' ')
+      fi
+    '';
+  };
 }
