7573be1e999b065a55f03f0b060e41bcba5fb466
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-10-04 22:49:46 +0800

    service(sillytavern): update

--- modules/services/ai/sillytavern/default.nix
+++ modules/services/ai/sillytavern/default.nix
@@ -1,11 +1,28 @@
 { pkgs, ... }:
-{
+let
+  user = "sillytavern";
+  group = user;
+in {
   imports = [
-    ./proxy.nix
+    # ./proxy.nix
   ];
 
-  # FIXME: impure implement
-  # Initialize sillytavern and rsync data directory and config.yaml to state directory
+  systemd.tmpfiles.settings.sillytavern = {
+    "/var/lib/SillyTavern/data".d = {
+      mode = "0700";
+      inherit user group;
+    };
+    "/var/lib/SillyTavern/extensions".d = {
+      mode = "0700";
+      inherit user group;
+    };
+    "/var/lib/SillyTavern/config.yaml"."L+" = {
+      mode = "0600";
+      argument = "${pkgs.sillytavern}/lib/node_modules/sillytavern/config.yaml";
+      inherit user group;
+    };
+  };
+
   systemd.services.sillytavern = {
     description = "SillyTavern";
     after = [ "network.target" ];
@@ -13,55 +30,40 @@
     path = with pkgs; [
       git
     ];
+    environment.XDG_DATA_HOME = "%S";
     serviceConfig = {
       Type = "simple";
-      ExecStartPre = [
-        "${pkgs.coreutils}/bin/cp -f ${./config.yaml} /var/lib/sillytavern/config.yaml"
-        "${pkgs.coreutils}/bin/chmod +w /var/lib/sillytavern/config.yaml"
+      ExecStart = "${pkgs.sillytavern}/bin/sillytavern";
+      User = user;
+      Group = group;
+      StateDirectory = "SillyTavern";
+      BindPaths = [
+        "/var/lib/SillyTavern/extensions:${pkgs.sillytavern}/lib/node_modules/sillytavern/public/scripts/extensions/third-party"
       ];
-      ExecStart = "${pkgs.sillytavern}/bin/sillytavern --dataRoot /var/lib/sillytavern/data";
-      User = "sillytavern";
-      Group = "sillytavern";
-      StateDirectory = "sillytavern";
-      WorkingDirectory = "/var/lib/sillytavern";
 
       Restart = "on-failure";
       RestartSec = "3s";
 
-      CapabilityBoundingSet = [ "" ];
-      LockPersonality = true;
-      NoNewPrivileges = true;
-      PrivateDevices = true;
-      PrivateTmp = true;
-      ProtectClock = true;
-      ProtectControlGroups = true;
-      ProtectHome = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectProc = "invisible";
-      ProtectSystem = "strict";
-      # RestrictAddressFamilies = [
-      #   "AF_UNIX"
-      #   "AF_INET"
-      #   "AF_INET6"
-      # ];
-      # RestrictNamespaces = true;
-      # RestrictRealtime = true;
-      # RestrictSUIDSGID = true;
-      # SystemCallArchitectures = "native";
-      # SystemCallFilter = [
-      #   "@system-service"
-      #   "@pkey"
-      #   "~@privileged"
-      # ];
+      #CapabilityBoundingSet = [ "" ];
+      #LockPersonality = true;
+      #NoNewPrivileges = true;
+#       PrivateDevices = true;
+#       PrivateTmp = true;
+#       ProtectClock = true;
+#       ProtectControlGroups = true;
+#       ProtectHome = true;
+#       ProtectHostname = true;
+#       ProtectKernelLogs = true;
+#       ProtectKernelModules = true;
+#       ProtectKernelTunables = true;
+#       ProtectProc = "invisible";
+#       ProtectSystem = "strict";
     };
   };
 
-  users.users.sillytavern = {
-    group = "sillytavern";
+  users.users."${user}" = {
+    inherit group;
     isSystemUser = true;
   };
-  users.groups.sillytavern = {};
+  users.groups."${group}"= {};
 }
