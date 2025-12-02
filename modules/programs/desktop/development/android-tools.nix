0565cedcbb39263b6b5a1707a0d9b4f1ff5b8450
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-12-02 12:20:18 +0800

    programs(desktop): remove unnecessary pkgs

--- modules/programs/desktop/development/android-tools.nix
+++ modules/programs/desktop/development/android-tools.nix
@@ -2,7 +2,6 @@
   nixosModule = { pkgs, user, lib, ... }: {
     # Android-Tools
     programs.adb.enable = true;
-    services.udev.packages = [ pkgs.android-udev-rules ];
 
     environment.persistence = lib.mkPersistDirsTree user [
       ".android"
--- modules/programs/desktop/misc/fcitx5/default.nix
+++ modules/programs/desktop/misc/fcitx5/default.nix
@@ -11,11 +11,11 @@
       i18n.inputMethod = {
         enable = true;
         type = "fcitx5";
-        fcitx5.addons = with pkgs; [
+        fcitx5.addons = with pkgs; with kdePackages; [
           fcitx5-chinese-addons
           fcitx5-gtk
           fcitx5-lua
-          kdePackages.fcitx5-qt
+          fcitx5-qt
           fcitx5-pinyin-zhwiki
           fcitx5-pinyin-moegirl
         ];
