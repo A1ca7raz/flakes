0b22287346caabea27eb600609f1a893d72bebf9
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-12-02 12:18:11 +0800

    nix: add nix-community cache

--- modules/nix/mirrors.nix
+++ modules/nix/mirrors.nix
@@ -4,9 +4,14 @@ let
   mkEduMirror = x: mkMirror "mirrors.${x}.edu.cn";
 in {
   nix.settings.substituters = [
+    "https://nix-community.cachix.org"
     (mkEduMirror "ustc")
     (mkEduMirror "bfsu")
 #     (mkEduMirror "sjtug.sjtu")
 #     (mkEduMirror "nju")
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 }
