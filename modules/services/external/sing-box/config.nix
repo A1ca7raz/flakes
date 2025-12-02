f0d78ec1ea08fa71eba4e5d9676edb1cde691f20
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-12-02 12:25:53 +0800

    service(sing-box): add trojan inbound

--- modules/services/external/sing-box/config.nix
+++ modules/services/external/sing-box/config.nix
@@ -4,11 +4,27 @@ let
   enc_host = { path = secrets.hosts.${const.node.profileName}; };
 
   dec = entry: { _secret = config.sops.secrets."${entry}".path; };
+
+  tls = {
+    alpn = [
+      "h3"
+      "h2"
+      "http/1.1"
+    ];
+    enabled = true;
+    server_name = dec "host/proxy/sni";
+    acme = {
+      domain = dec "host/proxy/sni";
+      email = dec "host/proxy/email";
+    };
+  };
 in {
   utils.secrets = {
     "inbounds/hysteria2/port" = enc_singbox;
     "inbounds/hysteria2/hopping_ports" = enc_singbox;
     "inbounds/hysteria2/secrets" = enc_singbox;
+    "inbounds/trojan/port" = enc_singbox;
+    "inbounds/trojan/secrets" = enc_singbox;
     "host/proxy/sni" = enc_host;
     "host/proxy/email" = enc_host;
   };
@@ -16,17 +32,29 @@ in {
   services.sing-box.settings.inbounds = [
     {
       type = "hysteria2";
-      listen  = "::";
+      listen = "::";
       listen_port = dec "inbounds/hysteria2/port" // { quote = false; };  # Integer
       users = dec "inbounds/hysteria2/secrets" // { quote = false; };     # Array
-      tls = {
-        alpn = [ "h3" ];
+      # udp_fragment = true;
+      inherit tls;
+    }
+
+    {
+      type = "trojan";
+      listen = "::";
+      listen_port = dec "inbounds/trojan/port" // { quote = false; };
+      users = dec "inbounds/trojan/secrets" // { quote = false; };
+      tcp_fast_open = true;
+      tcp_multi_path = true;
+      # udp_fragment = true;
+      multiplex = {
         enabled = true;
-        server_name = dec "host/proxy/sni";
-        acme = {
-          domain = dec "host/proxy/sni";
-          email = dec "host/proxy/email";
-        };
+        padding = true;
+      };
+      inherit tls;
+      transport = {
+        type = "ws";
+        path = "";
       };
     }
   ];
