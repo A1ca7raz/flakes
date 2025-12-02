fc5cb600768b299e029b4993fd2e47a4cb28e937
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-12-03 01:04:19 +0800

    program(git): add aliases

--- modules/programs/desktop/development/git.nix
+++ modules/programs/desktop/development/git.nix
@@ -4,5 +4,12 @@
     enable = true;
     userName = "A1ca7raz";
     userEmail = "7345998+A1ca7raz@users.noreply.github.com";
+
+    settings.alias = {
+      br = "branch";
+      ci = "commit";
+      co = "checkout";
+      st = "status";
+    };
   };
 }
